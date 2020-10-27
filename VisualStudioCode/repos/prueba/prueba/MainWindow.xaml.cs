6d426d28dfd3764aa457bff84e577dd852346d2f
Jose Antonio Velasco <57660135+JoseAntonioVelasco@users.noreply.github.com>
2020-10-27 13:06:27 +0100

    27/10/2020 clase
    blend xaml

---
+++ VisualStudioCode/repos/prueba/prueba/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prueba
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Keydown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("keyDown boton");
+        }
+
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("keyDown grid");
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("keyDown window");
+        }
+
+        private void Button_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("PreviewkeyDown boton");
+        }
+
+        private void Grid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("PreviewkeyDown grid");
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("PreviewkeyDown window");
+        }
+    }
+}

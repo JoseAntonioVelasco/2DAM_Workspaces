f510eb9d818845f1f157cc0303dd275fe97f1459
Jose Antonio Velasco <57660135+JoseAntonioVelasco@users.noreply.github.com>
2020-12-10 13:04:54 +0100

    10/12/2020 clase

---
+++ Python/paquetes/scripts/prueba.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 10 09:03:21 2020
+
+@author: ADMIN
+"""
+
+from paquete import suma
+print("prueba")
+suma.suma()
---
+++ Python/paquetes/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 10 08:27:27 2020
+
+@author: ADMIN
+"""
+
+from setuptools import setup
+
+setup(
+      name="PaqueteDeJose",
+      version="0.1",
+      description="Este es un paquete de prueba para SGE",
+      author="jose",
+      author_email="joseantonio.velasco@sanviatorvalladolid.com",
+      packages=["paquete"]
+)

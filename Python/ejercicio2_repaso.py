8c986067fee90beedea572237016afca65211516
Jose <joseantonio.velasco@sanviatorvalladolid.com>
2021-01-25 23:00:52 +0100

    25/01/2021 casa

--- Python/ejercicio2_repaso.py
+++ Python/ejercicio2_repaso.py
@@ -9,7 +9,7 @@ import random
 
 class Jugador:
     def __init__(self,nombre = 0,apellido1 = 0,apellido2 = 0, nombreCompletoEquipo = 0,
-                 PJ = 0, PT = 0, puntos = 0, asis = 0, reb = 0, tap = 0, vN = []):
+                 PJ = 0, PT = 0, puntos = 0, asis = 0, reb = 0, tap = 0,vN=None):
         self.nombre = nombre
         self.apellido1 = apellido1
         self.apellido2 = apellido2
@@ -20,10 +20,10 @@ class Jugador:
         self.asis = asis
         self.reb = reb
         self.tap = tap
-        self.vN = vN
+        self.vN = []
     
 class Equipo:
-    def __init__(self,nombre=0,presupuesto=0,n_jugadores=0,pg=0,pe=0,pp=0,pf=0,pc=0,jugadores = []):
+    def __init__(self,nombre=0,presupuesto=0,n_jugadores=0,pg=0,pe=0,pp=0,pf=0,pc=0,jugadores=None):
         self.nombre = nombre
         self.presupuesto = presupuesto
         self.n_jugadores = n_jugadores
@@ -32,7 +32,7 @@ class Equipo:
         self.pp = pp
         self.pf = pf
         self.pc = pc
-        self.jugadores = jugadores
+        self.jugadores = []
     def playMatch(self,equipoOponente):
         #Win prob 49.5%
         #Lose prob 49.5%
@@ -78,18 +78,12 @@ with open('equipos.csv') as csv_file:
 
 #meto jugadores al equipo
 for equipo in listaEquipos:
-    listaJugadoresEquipo = []
     for i in range(int(equipo.n_jugadores)):
         jugador = Jugador()
         genDataJug(jugador)
         jugador.nombreCompletoEquipo=equipo.nombre
-        listaJugadoresEquipo.append(jugador)
-    print("id equipo.jugadores: "+str(id(equipo.jugadores)))
-    print("id listaJugadores: "+str(id(listaJugadoresEquipo)))
-    print("id equipo: "+str(id(equipo)))
-    print()
-    equipo.jugadores = listaJugadoresEquipo
-
+        equipo.jugadores.append(jugador)
+        
 def indiceRepetido(listaIndices,indice):
     for index in listaIndices:
         if index==indice:
@@ -113,17 +107,19 @@ for equipo in listaEquipos:
         for j in range(njugadores_juegan):
             indice = genIndice()
             listaIndices.append(indice)
-            
+        
         for indice in listaIndices:
             equipo.jugadores[indice].puntos = equipo.jugadores[indice].puntos + random.randint(0,30)
             equipo.jugadores[indice].asis = equipo.jugadores[indice].asis + random.randint(0,15)
             equipo.jugadores[indice].reb = equipo.jugadores[indice].reb + random.randint(0,15)
             equipo.jugadores[indice].tap = equipo.jugadores[indice].tap + random.randint(0,5)
-            equipo.jugadores[indice].vN.append(random.randint(-30,40)) 
+            equipo.jugadores[indice].vN.append(random.randint(-30,40))
+            
         #jugadores que no han jugado el partido
         for j in range(int(equipo.n_jugadores)):
             if j not in listaIndices:
-                equipo.jugadores[indice].vN.append(0)
+                equipo.jugadores[j].vN.append(0)
+        
 
 #relleno la lista jugadores para crear el csv con todos los jugadores de todos
 #los equipos
@@ -132,6 +128,7 @@ for equipo in listaEquipos:
     jugadores = equipo.jugadores
     for jugador in jugadores:
         listaJugadores.append(jugador)
+        #print(len(jugador.vN))
 
 with open('jugadores.csv','w',newline='') as file:
     writer = csv.writer(file,delimiter=';')
